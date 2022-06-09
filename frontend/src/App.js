5385d2caab19a8bcad6b882a72b809562cf7407e
thaott1901 <thaott1901@gmail.com>
2022-06-09 01:19:45 -0400

    Convert to react

---
+++ frontend/src/App.js
@@ -0,0 +1,96 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+  return (
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <a href="index.html">Akali</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Signin</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Pants</a>
+                    </li>
+
+                    <li>
+                        <a href="index.html">Shirts</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <ul className="products">
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d1.jpg" alt="Product" />
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d1.jpg" alt="Product" />
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d1.jpg" alt="Product" />
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src="/images/d1.jpg" alt="Product" />
+                                <div className="product-name">
+                                    <a href="product.html">Slim Shirt</a>
+                                </div>
+                                <div className="product-brand">Nike</div>
+                                <div className="product-price">$60</div>
+                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>  
+            </main>
+            <footer className="footer">
+                All right reserved.
+            </footer>
+        </div>
+  );
+}
+
+export default App;
