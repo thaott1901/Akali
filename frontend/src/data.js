f26236f15e1a7dcd88b95b786db83784303a8dd2
thaott1901 <thaott1901@gmail.com>
2022-06-10 01:14:47 -0400

    Dynamic data render using jsx

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products:[
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image:'/images/d1.jpg',
+            price: 50,
+            brand: 'Uniqlo',
+            rating: 4.0,
+            numReviews: 9
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image:'/images/d1.jpg',
+            price: 70,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 8
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image:'/images/d1.jpg',
+            price: 70,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 8
+        },
+    ]
+}
